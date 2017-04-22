9a1040254ffd09d6bd10c6bdfa192b4feb26c7d0
Tim Ehlers <tehlers@gwdg.de>
2017-04-22 18:17:22 +0200

    fix: unknown files excluded

--- filetree.py
+++ filetree.py
@@ -83,7 +83,7 @@ def tracing(a):
             tracing(os.path.join(a, d))
             print ("</ul></li>\n")
     for f in sorted(files):
-        if select_icon(f) != "glyphicon-leaf":
+        if select_icon(f) != "glyphicon glyphicon-leaf":
             try:
                 print ("<li data-path=\"", get_filepathlink(a, f), "\" title=\"Size: ", human_size(os.path.getsize(os.path.join(a, f))), "\" data-jstree='{\"icon\":\"", select_icon(f), "\"}'", "onclick=\"window.location.href='", a, "/", f, "';\" style=\"cursor:pointer;\">", "<a href=", urllib.parse.quote(a), "/", urllib.parse.quote(f), ">", f, "</a></li>\n",sep="")
             except UnicodeEncodeError:
